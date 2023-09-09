7559e03bca908238f830c07e4cbd751a79ee76d5
Sophia Phu <sophiatiffphu@gmail.com>
2023-09-09 16:20:08 -0500

    updated inputs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Menubar from './components/Menubar';
+import InputFields from './components/InputFields';
 
 function App() {
   return (
     <div className="App">
       <Menubar/>
+      <InputFields/>
     </div>
   );
 }
