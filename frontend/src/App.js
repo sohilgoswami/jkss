554ffa3980a3a5f4b8cf811f7a02376de2f0aa3e
Sophia Phu <sophiatiffphu@gmail.com>
2023-09-09 21:58:11 -0500

    create table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Menubar from './components/Menubar';
 import InputFields from './components/InputFields';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ViewGrades from './components/ViewGrades';
 
 function App() {
   return (
     <div className="App">
       <Menubar/>
-      <InputFields/>
+      <BrowserRouter>
+        <Routes>
+          <Route path ="/" element={<InputFields/>} />
+          <Route path = "/viewGrades" element={<ViewGrades/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
